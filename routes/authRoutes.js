af30a3f79f75adadf0600e4b736cd5d1bad63d91
Dharunkumar <dharunkumar.n2024cse@sece.ac.in>
2025-12-19 09:57:57 +0530

    frontend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authController = require('../controller/authcontroller');
+const authController = require('../controller/authController');
 const authRoutes = express.Router();
 
 authRoutes.post('/login', authController.login);
